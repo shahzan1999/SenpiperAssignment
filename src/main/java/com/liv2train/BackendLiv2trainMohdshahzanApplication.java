4edfbade79ccff646679812086ddd3479ce1d0f2
Mohd Shahzan <shahzan1999@gmail.com>
2021-07-21 18:32:59 +0530

    -initial setup

---
+++ src/main/java/com/liv2train/BackendLiv2trainMohdshahzanApplication.java
@@ -0,0 +1,13 @@
+package com.liv2train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLiv2trainMohdshahzanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLiv2trainMohdshahzanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liv2train/BackendLiv2trainMohdshahzanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liv2train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLiv2trainMohdshahzanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
